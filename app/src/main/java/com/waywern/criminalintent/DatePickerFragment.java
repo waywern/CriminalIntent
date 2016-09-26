fbf8157d9620ebf2e7332148944a1e4ffbbdcc1d
MOA.MIGHTY\slisovtsov <waywern@gmail.com>
2016-09-26 13:35:14 -0400

    Prepare for dialog for picking the date of a crime. Draw DatePicker and add layout to onclick listener.

--- app/src/main/java/com/waywern/criminalintent/DatePickerFragment.java
+++ app/src/main/java/com/waywern/criminalintent/DatePickerFragment.java
@@ -4,6 +4,7 @@ import android.app.Dialog;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
+import android.view.View;
 
 /**
  * Created by slisovtsov on 9/26/2016.
@@ -12,7 +13,15 @@ public class DatePickerFragment extends DialogFragment {
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
+        /*return new AlertDialog.Builder(getActivity())
+                .setTitle(R.string.date_picker_title)
+                .setPositiveButton(android.R.string.ok, null)
+                .create();*/
+        View v = getActivity().getLayoutInflater()
+                .inflate(R.layout.dialog_date, null);
+
         return new AlertDialog.Builder(getActivity())
+                .setView(v)
                 .setTitle(R.string.date_picker_title)
                 .setPositiveButton(android.R.string.ok, null)
                 .create();
