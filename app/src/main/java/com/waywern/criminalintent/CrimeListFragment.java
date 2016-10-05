9a06c252dc99e4c5ac95a1543d85d440c6aeab4c
MOA.MIGHTY\slisovtsov <waywern@gmail.com>
2016-10-05 14:24:22 -0400

    Try to add action bar (menu bar). Unsuccessfully(

--- app/src/main/java/com/waywern/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/waywern/criminalintent/CrimeListFragment.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -22,6 +24,7 @@ public class CrimeListFragment extends ListFragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
         getActivity().setTitle(R.string.crimes_title);
         mCrimes = CrimeLab.get(getActivity()).getmCrimes();
 
@@ -75,6 +78,13 @@ public class CrimeListFragment extends ListFragment {
     public void onResume() {
         super.onResume();
         ((CrimeAdapter)getListAdapter()).notifyDataSetChanged();
+
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.fragment_crime_list, menu);
 
     }
 }
